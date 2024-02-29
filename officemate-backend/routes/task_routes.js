5121369c3100d3f97cf4de5c3816cb1cb065aa05
Hosanna Martin <vivahosanna@icloud.com>
2024-02-29 23:21:16 +0800

    task update

--- officemate-backend/routes/task_routes.js
+++ officemate-backend/routes/task_routes.js
@@ -6,13 +6,13 @@ const auth = require("../middlewares/security");
 
 router.post("/create", auth.checkJWT, tasksController.newTask);
 
-// /* === ROUTE GET All NOTE === */
+// /* === ROUTE GET All TASK === */
 router.get("/note/:id", auth.checkJWT, tasksController.getAllTasksOfNote);
 
 router.get("/one/:id", auth.checkJWT, tasksController.getOneTask);
 
 // /* === ROUTE UPDATE NOTE === */
-router.patch("/:id", auth.checkJWT, tasksController.updateOneNote);
+router.patch("/:id", auth.checkJWT, tasksController.updateTask);
 
 // /* === ROUTE DELETE NOTE === */
 router.delete("/:id", auth.checkJWT, tasksController.deleteTask);
